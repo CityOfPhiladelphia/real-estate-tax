6058a027742a745a03241c949c3ac20fc66111f5
andy.rothwell <rothwell.andy@gmail.com>
2018-07-31 09:42:25 -0400

    fixes "balance details" and clears "payments agreements"

--- src/topics/balance.js
+++ src/topics/balance.js
@@ -6,145 +6,95 @@ export default {
   dataSources: ['opa'],
   components: [
     {
-      type: 'callout',
-      slots: {
-        text: '\
-          Property assessment and sale information for this address. Source: Office of Property Assessments (OPA). OPA was formerly a part of the Bureau of Revision of Taxes (BRT) and some City records may still use that name.\
-        '
-      }
-    },
-    {
-      type: 'vertical-table',
-      slots: {
+      type: 'horizontal-table',
+      options: {
+        id: 'balanceDetails',
+        limit: 5,
         fields: [
           {
-            label: 'OPA Account #',
-            value: function(state) {
-              return state.geocode.data.properties.opa_account_num;
-            }
-          },
-          {
-            label: 'OPA Address',
-            value: function(state) {
-              return state.geocode.data.properties.opa_address;
-            }
-          },
-          {
-            label: 'Owners',
-            value: function(state) {
-              var owners = state.geocode.data.properties.opa_owners;
-              var ownersJoined = owners.join(', ');
-              return ownersJoined;
-            }
-          },
-          {
-            label: 'Assessed Value',// + new Date().getFullYear(),
-            value: function(state) {
-              var data = state.sources.opa.data;
-              // return data.market_value;
-              var result;
-              if (data) {
-                result = data.market_value;
-              } else {
-                result = 'no data';
-              }
-              return result;
+            label: 'Year',
+            value: function(state, item){
+              return item.most_recent_year_owed;
             },
-            transforms: [
-              'currency'
-            ]
-          },
-          {
-            label: 'Sale Date',
-            value: function(state) {
-              var data = state.sources.opa.data;
-              // return data.sale_date;
-              var result;
-              if (data) {
-                result = data.sale_date;
-              } else {
-                result = 'no data';
-              }
-              return result;
-            },
+            nullValue: 'no date available',
             transforms: [
               'date'
             ]
           },
           {
-            label: 'Sale Price',
-            value: function(state) {
-              var data = state.sources.opa.data;
-              // return data.sale_price;
-              var result;
-              if (data) {
-                result = data.sale_price;
-              } else {
-                result = 'no data';
-              }
-              return result;
-            },
-            transforms: [
-              'currency'
-            ]
+            label: 'Principal',
+            value: function(state, item){
+              return item.principal_due;
+            }
+          },
+          {
+            label: 'Interest',
+            value: function(state, item){
+              return item.interest_due;
+            }
+          },
+          {
+            label: 'Penalty',
+            value: function(state, item){
+              return item.penalty_due;
+            }
+          },
+          {
+            label: 'Other',
+            value: function(state, item){
+              return item.other_charges_due;
+            }
+          },
+          {
+            label: 'Total',
+            value: function(state, item){
+              return item.total_due;
+            }
+          },
+          {
+            label: 'Owner',
+            value: function(state, item){
+              return item.owner;
+            }
+          },
+          {
+            label: 'Status',
+            value: function(state, item){
+              return item.agreement_agency;
+            }
           },
         ],
-      },
-      options: {
-        id: 'opaData',
-        // requiredSources: ['opa'],
-        externalLink: {
-          action: function(count) {
-            return 'See more';
+        sort: {
+          // this should return the val to sort on
+          getValue: function(item) {
+            return item.permitissuedate;
           },
-          name: 'Property Search',
-          href: function(state) {
-            var id = state.geocode.data.properties.opa_account_num;
-            return 'http://property.phila.gov/?p=' + id;
-          }
-        }
-      }
-    }
-
-    //     ]
-    //   }
-    // }
+          // asc or desc
+          order: 'desc'
+        },
+        // externalLink: {
+        //   action: function(count) {
+        //     return 'See ' + count + ' older permits at L&I Property History';
+        //   },
+        //   name: 'L&I Property History',
+        //   href: function(state) {
+        //     var address = state.geocode.data.properties.street_address;
+        //     var addressEncoded = encodeURIComponent(address);
+        //     return 'http://li.phila.gov/#summary?address=' + addressEncoded;
+        //   }
+        // }
+      },
+      slots: {
+        title: 'Balance Details',
+        items: function(state) {
+          var data = state.sources['realEstateTaxDelinquencies'].data.rows;
+          var rows = data.map(function(row){
+            var itemRow = row;
+            return itemRow;
+          });
+          return rows;
+        },
+      },
+    },
   ],
-  basemap: 'pwd',
-  identifyFeature: 'address-marker',
-  // we might not need this anymore, now that we have identifyFeature
-  parcels: 'pwd',
-  errorMessage: function (state) {
-    var data = state.sources.condoList.data;
-        // features = data.features;
-
-    if (data) {
-      var numCondos = data.total_size;
-
-      if (numCondos > 0) {
-        var shouldPluralize = numCondos > 1,
-            isOrAre = shouldPluralize ? 'are' : 'is',
-            unitOrUnits = shouldPluralize ? 'units' : 'unit',
-            message = [
-              '<h3>',
-              'There ',
-              isOrAre,
-              // ' <strong>',
-              ' ',
-              numCondos,
-              ' condominium ',
-              unitOrUnits,
-              // '</strong> at this address.</h3>',
-              ' at this address.</h3>',
-              // ' at this address. ',
-              '<p>You can use the Condominiums tab below to see information for an individual unit.</p>'
-              // 'Please select a unit from the Condominiums tab below.'
-            ].join('');
-
-        return message;
-      }
-    } else {
-      return 'There is no property assessment record for this address.';
-    }
-  }
 }
