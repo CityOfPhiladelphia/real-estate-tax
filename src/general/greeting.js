aa2a3019e6938c6fda9c28744b8ea2b93320d40a
andy.rothwell <rothwell.andy@gmail.com>
2018-11-14 11:39:28 -0500

    changes to greeting, fixes 1 of 2 typeforms

--- src/general/greeting.js
+++ src/general/greeting.js
@@ -1,17 +1,16 @@
 export default {
   initialMessage: '\
-    <h2><b>About this payment</b></h2>\
+    <h2><b>About the Philadelphia Real Estate Tax</b></h2>\
     <div class="callout">\
       <p>The City and the School District of Philadelphia impose a tax on all real estate in the City pursuant to Philadelphia\
         Code Chapter 19-1300, as authorized by 72 P.S. § 5020-201. The Office of Property Assessment (OPA) determines\
         the value of the property on which the taxes must be paid.</p><br>\
       <p>Real Estate Tax bills are sent in December for the following year and payments are due March 31st. If you pay on\
         or before the last day of February, you receive a 1% discount. If you pay after March 31, you are subject to\
-        increased charges called "additions". At the end of the year, these charges begin to accrue taxes and penalties.</p><br>\
+        increased charges called "additions." At the end of the year, these charges begin to accrue interest and penalties.\
+        <a target="_blank" href="https://www.phila.gov/services/property-lots-housing/property-taxes/">Find\
+        more information about Philadelphia Real Estate Tax</a>, including information about discount and assistance programs.</p><br>\
       <p>For questions about your account, email <a href="mailto:revenue@phila.gov">revenue@phila.gov</a> or call 215-686-6442.</p><br>\
-      <p>For questions about account payoffs, email <a href="mailto:retaxpayoff@phila.gov">retaxpayoff@phila.gov</a>. To receive payoff amounts, please email\
-        the statement of claim number from the legal action, the property address and/or the Office of Property\
-        Assessment number to <a href="mailto:retaxpayoff@phila.gov">retaxpayoff@phila.gov</a> or fax it to 215-686-0582.</p><br>\
     </div>\
   ',
 }
