68e94351aec7b426c9c2a4481fbb208d7f7b333d
andy.rothwell <rothwell.andy@gmail.com>
2018-11-20 15:33:58 -0500

    sets phone numbers in popovers to be a good format for consistency

--- src/general/transforms.js
+++ src/general/transforms.js
@@ -7,15 +7,15 @@ export default {
   statusMap: {
     transform: function(value) {
       var map = {
-        'LBR': 'Collections being pursued by Linebarger Collection Agency. Please call 215-790-1117.',
+        'LBR': 'Collections being pursued by Linebarger Collection Agency. Please call (215) 790-1117.',
         'LBRA': 'In an agreement with Linebarger.',
         'LBRO': 'In an Owner Occupied Payment Arrangement (OOPA) agreement with LBR.',
-        'GRB': 'Collections being pursued by GRB Collection Agency. Please call 866-677-5970.',
+        'GRB': 'Collections being pursued by GRB Collection Agency. Please call (866) 677-5970.',
         'GRBA': 'In an agreement with GRB.',
         'GRBO': 'In an Owner Occupied Payment Arrangement (OOPA) agreement with GRB.',
         'AGRE': 'Active payment agreement plan in effect for delinquent taxes.',
         'INST': 'Active installment payment plan in effect for current taxes.',
-        'PIO': 'Collections being pursued by Pioneer Collection Agency. Please call 866-439-1318.',
+        'PIO': 'Collections being pursued by Pioneer Collection Agency. Please call (866) 439-1318.',
         'BRT': 'Under appeal with the Board of Revision of Taxes.',
         'LSLD': `This charge has a tax lien that was sold to a private third-party lien-holder at a City Tax Lien Sale.
                 Therefore, the amounts listed below for those years may not include all Real Estate Tax liabilities currently due.
@@ -25,7 +25,7 @@ export default {
                 You may enter or copy/paste the 13-digit lien number listed below (ex. 1504R14010000) to the Court’s “Case ID” search box.`,
         'DISC': 'Discounted amount good until March 1st.',
         'SEQR': `This charge has a tax lien that is in the Law Department\'s Sequestration/Receivership Program. For more information regarding the status
-                of the sequestration proceedings, you may call 215-686-3629, or search the Philadelphia Court's civil docket
+                of the sequestration proceedings, you may call (215) 686-3629, or search the Philadelphia Court's civil docket
                 at <a href="http://fjdefile.phila.gov/efsfjd/zk_fjd_public_qry_00.zp_disclaimer" target="_blank">Civil Docket Access</a>.
                 You may enter the property owner's name in the Court’s “Caption” search box.`,
       }
