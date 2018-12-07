09b9a0ccfb61150eca7343adf3a400684a57a3e0
andy.rothwell <rothwell.andy@gmail.com>
2018-12-07 09:42:10 -0500

    changes url to dev for print payment coupon

--- src/components/propertyCallout.vue
+++ src/components/propertyCallout.vue
@@ -95,7 +95,7 @@
         window.open('https://www.phila.gov/services/payments-assistance-taxes/payment-plans/', '_blank');
       },
       couponButtonAction() {
-        window.open('https://ework.phila.gov/revenue/', '_blank');
+        window.open('http://revtestegov.phila.dmz/default?realestate=true', '_blank');
       },
     }
   };
