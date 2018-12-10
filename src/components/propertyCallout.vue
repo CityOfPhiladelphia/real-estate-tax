6e5c6c030587e0aac1b7f27508181024a6f7a249
andy.rothwell <rothwell.andy@gmail.com>
2018-12-10 15:14:43 -0500

    fixes coupon url to old one, removes new relic info

--- src/components/propertyCallout.vue
+++ src/components/propertyCallout.vue
@@ -95,7 +95,7 @@
         window.open('https://www.phila.gov/services/payments-assistance-taxes/payment-plans/', '_blank');
       },
       couponButtonAction() {
-        window.open(' https://ework.phila.gov/revenue/Default.asp?coupon=yes', '_blank');
+        window.open('https://ework.phila.gov/revenue/default?realestate=true', '_blank');
       },
     }
   };
