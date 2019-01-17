5df824eefef5c0a3fbae6860ae8985fc42f91171
andy.rothwell <rothwell.andy@gmail.com>
2019-01-17 10:23:01 -0500

    uses new commit of mapboard which fixes bugs with "showTopicsBasedOnOtherData"

--- src/topics/property.js
+++ src/topics/property.js
@@ -9,9 +9,6 @@ export default {
   label: 'Property Information',
   // dataSources: ['opa'],
   dataSources: ['opa', 'tips'],
-  errorMessage: function() {
-    return 'Could not locate tax records for that address.';
-  },
   components: [
     {
       type: 'propertyCallout',
@@ -108,4 +105,37 @@ export default {
       }
     }
   ],
+  errorMessage: function (state) {
+    var data = state.sources.condoList.data;
+        // features = data.features;
+
+    if (data) {
+      var numCondos = data.total_size;
+
+      if (numCondos > 0) {
+        var shouldPluralize = numCondos > 1,
+            isOrAre = shouldPluralize ? 'are' : 'is',
+            unitOrUnits = shouldPluralize ? 'units' : 'unit',
+            message = [
+              '<h3>',
+              'There ',
+              isOrAre,
+              // ' <strong>',
+              ' ',
+              numCondos,
+              ' condominium ',
+              unitOrUnits,
+              // '</strong> at this address.</h3>',
+              ' at this address.</h3>',
+              // ' at this address. ',
+              '<p>You can use the Condominiums tab above to see information for an individual unit.</p>'
+              // 'Please select a unit from the Condominiums tab below.'
+            ].join('');
+
+        return message;
+      }
+    } else {
+      return 'There is no property assessment record for this address.';
+    }
+  }
 }
