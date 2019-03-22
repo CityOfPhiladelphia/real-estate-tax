66858070cdb0c7858d4cab40005cad47eb440776
andy.rothwell <rothwell.andy@gmail.com>
2019-03-22 10:45:07 -0400

    uses mapboard 3.0.31, adds maintenance note

--- src/components/newSiteModal.vue
+++ src/components/newSiteModal.vue
@@ -5,9 +5,9 @@
       <div class="main-div"
            :style="minWidth"
       >
-        <!-- <div class="maintenance-div">
-          <p>Note: The pay option on this site will be down for scheduled maintenance from 10:00 pm on 2/12/2019 to 8:00 am on 2/13/2019</p>
-        </div> -->
+        <div class="maintenance-div">
+          <p>Note: This site may down at different points for scheduled maintenance from 8:00 am to 12:00 pm on Sunday, 3/24/2019</p>
+        </div>
         <div class="top-div small-centered">
           <h2>Welcome to the new<br><b>Real Estate Tax Balance Search!</b></h2>
         </div>
