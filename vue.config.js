f9da96f1d032feb75be4497ae74f1e4546cc55e9
andy.rothwell <rothwell.andy@gmail.com>
2019-04-10 09:47:56 -0400

    adds path "revenue/realestatetax/"

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const Visualizer = require('webpack-visualizer-plugin');
 
 module.exports = {
-  publicPath: undefined,
+  publicPath: '/revenue/realestatetax/',
 
   configureWebpack: {
     plugins: [
