0954ce5bd845b8d3347161631775c3506c73e569
Dan Lopez <dan.lopez@phila.gov>
2019-07-31 13:43:46 -0400

    update url

--- src/components/propertyCallout.vue
+++ src/components/propertyCallout.vue
@@ -67,7 +67,7 @@
           width: 160,
           fontSize: 25,
           // actionAddress: 'https://test-secure.phila.gov/paymentcenter/gateway1/initiatepurchase.aspx'
-          actionAddress: 'https://test-epay.phila.gov/paymentcenter/gateway1/initiatepurchase.aspx'
+          actionAddress: 'http://localhost:21579/paymentcenter/gateway1/initiatepurchase.aspx'
           // actionAddress: 'https://secure.phila.gov/PaymentCenter/Gateway1/InitiatePurchase.aspx'
         }
         return options;
